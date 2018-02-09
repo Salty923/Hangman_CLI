d3737fe56479bbf88b7bfcba6310f8229d06e2ea
Scott Saltzman <salty923@gmail.com>
2018-02-09 08:48:54 -0600

    psudo code basic functionality

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+//Show user the spaces for the random word chosen
+
+//ask the user to guess a letter
+
+//check to see if letter has been guessed already
+
+//check to see if letter matches
+
+//if letter matches, check to see if word is complete
+
+//if letter does not match, take away a guess
+
+//check to see if user has any more guesses if yes, continue
+
+//rerun show spaces and guess letter
+
